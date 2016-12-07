2562345681234ac744fb4c291315bdb376b6397a
Thornbury, Ruairi (UK - Belfast) <rthornbury@deloitte.co.uk>
2016-12-07 09:57:59 +0000

    2nd commit

--- Jenkins_Source_Code/src/hello_world/HelloWorld.java
+++ Jenkins_Source_Code/src/hello_world/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Ruairi ");
+		System.out.println("Hello Ruairi This has been changed ");
 
 	}
 
